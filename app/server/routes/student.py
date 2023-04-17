f05c5cad5224c1eef74bea8e6a8491cdd282c435
Edwar Garcia <garciapatinoedwar26@gmail.com>
2023-04-16 22:09:51 -0500

    Creacion del CRUD completo, y correcion de error en conexion con database

--- app/server/routes/student.py
+++ app/server/routes/student.py
@@ -14,3 +14,46 @@ async def add_student_data(student: StudentSchema = Body(...)):
     new_student = await add_student(student)
     return response_model(new_student, "Student added successfully") 
 
+
+@router.get("/", response_description="Students retrieved")
+async def get_students():
+    students = await retrieve_students()
+    if students:
+        return response_model(students, "Students data retrieved succesfully")
+    return response_model(students, "Empty list returned")
+    
+
+@router.get("/{id}", response_description="Student data retrieved")
+async def get_student_data(id):
+    student = await retrieve_student(id)
+    if student:
+        return response_model(student, "Student data retrieved succesfully")
+    return error_response_model("An error ocurred.",404, "Student doesn't exist")
+
+
+@router.put("/{id}")
+async def update_student_data(id: str, req: UpdateStudentModel = Body(...)):
+    req = {k:v for k, v in req.dict().items() if v is not None}
+    updated_student = await update_student(id, req)
+    if updated_student:
+        return response_model(
+            "Student with ID: {} name update is successful".format(id),
+            "Student name updated successfully",
+        )
+    return response_model(
+        "An error occurred",
+        404,
+        "There was an error updating the student data.",
+    )
+
+
+@router.delete("/{id}", response_description="Student data deleted from the database")
+async def delete_student_data(id: str):
+    deleted_student = await delete_student(id)
+    if deleted_student:
+        return response_model(
+            "Student with ID: {} removed".format(id), "Student deleted successfully"
+        )
+    return response_model(
+        "An error occurred", 404, "Student with id {0} doesn't exist".format(id)
+    )
